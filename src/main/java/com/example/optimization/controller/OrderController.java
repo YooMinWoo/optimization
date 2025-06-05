f89ac14e3bc4ece07fffb5c046c2ad459ec96c25
YooMinWoo <alsn99@codeline.kr>
2025-06-05 14:46:14 +0900

    엔드포인트 수정

--- src/main/java/com/example/optimization/controller/OrderController.java
+++ src/main/java/com/example/optimization/controller/OrderController.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class OrderController {
 
     private final OrderService orderService;
